8c56c46738fa30bb07a23cfb9027ac0053aad3ad
neveganv <victorianastiuk@gmail.com>
2021-11-19 15:08:12 +0200

    added axios and router

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+
+// router
+Vue.use(VueRouter)
+
+// axios
+Vue.use(VueAxios, axios)
 
 new Vue({
   render: h => h(App),
