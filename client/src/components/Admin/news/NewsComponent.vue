c6b3cc35b4239ca6f27a44c8de27427919825a66
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2021-11-21 16:01:50 +0200

    news tab

--- client/src/components/Admin/news/NewsComponent.vue
+++ client/src/components/Admin/news/NewsComponent.vue
@@ -1,18 +1,31 @@
 <template>
 	<div>
-    
-  </div>
+		<v-tabs v-model="tabs">
+			<v-tab href="#recentNews">Новини</v-tab>
+			<v-tab href="#simpleNews">Нещодавні новини</v-tab>
+		</v-tabs>
+		<v-tabs-items v-model="tabs">
+			<v-tab-item :value="'recentNews'">
+				<v-card flat>
+					<v-card-text>Проста крута новина</v-card-text>
+				</v-card>
+			</v-tab-item>
+			<v-tab-item :value="'simpleNews'">
+				<v-card flat>
+					<v-card-text> Нещодавна новина </v-card-text>
+				</v-card>
+			</v-tab-item>
+		</v-tabs-items>
+	</div>
 </template>
 
 <script>
 export default {
 	components: {},
 	data: () => ({
-
-  }),
+		tabs: null,
+	}),
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
