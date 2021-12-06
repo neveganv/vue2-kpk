473acffbd110456779953c481af045a6e2460929
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2021-12-06 18:00:47 +0200

    add view

--- client/src/components/Main/UI/my-news-card.vue
+++ client/src/components/Main/UI/my-news-card.vue
@@ -2,10 +2,15 @@
 	<div class="my-new">
 		<div class="my-new__header">
 			<b>{{ newItem.category.name || '--' }}</b>
-			<span> {{ moment(newItem.created_time).locale('uk').format('LL')   || '--'}} </span>
+			<div>
+				<span>
+					{{ moment(newItem.created_time).locale('uk').format('LL') || '--' }}
+				</span>
+				<div class="views"><VIcon small >mdi-eye</VIcon>99</div>
+			</div>
 		</div>
 		<div class="my-new__content">
-			{{newItem.title }}
+			{{ newItem.title }}
 		</div>
 		<div class="my-new__img">
 			<img :src="newItem.main_img" />
@@ -25,14 +30,14 @@ export default {
 
 <style lang="scss" scoped>
 .my-new {
-    cursor: pointer;
+	cursor: pointer;
 	max-width: 530px;
 	width: 100%;
 	height: 440px;
 	padding-left: 15px;
-    margin: 20px 0;
-    position: relative;
-    transition: .2s ease;
+	margin: 20px 0;
+	position: relative;
+	transition: 0.2s ease;
 	&__header {
 		display: flex;
 		justify-content: space-between;
@@ -51,10 +56,17 @@ export default {
 			line-height: 13px;
 			font-weight: 300;
 		}
+        .views{
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            font-size: 13px;
+            color:#757575
+        }
 	}
 	&__content {
 		margin-top: 20px;
-        margin-bottom: 10px;
+		margin-bottom: 10px;
 		font-weight: normal;
 		width: 80%;
 	}
@@ -63,25 +75,25 @@ export default {
 		width: 510px;
 
 		img {
-            object-fit: cover;
+			object-fit: cover;
 			width: 100%;
 			height: 100%;
 		}
 	}
-    &::after{
-        position: absolute;
-        content:'';
-        width: 2px;
-        left:0;
-        top: 0;
-        height: 80%;
-        background: #757575;;
-    }
-    &:hover{
-        opacity: .9;
-    }
-    &:active{
-        transform: scale(.95);
-    }
+	&::after {
+		position: absolute;
+		content: '';
+		width: 2px;
+		left: 0;
+		top: 0;
+		height: 80%;
+		background: #757575;
+	}
+	&:hover {
+		opacity: 0.9;
+	}
+	&:active {
+		transform: scale(0.95);
+	}
 }
 </style>
