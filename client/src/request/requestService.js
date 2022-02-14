d27e7238e2f31fb4bbc9ffc61282dcde69f779ef
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-02-14 13:11:22 +0200

    token security

--- client/src/request/requestService.js
+++ client/src/request/requestService.js
@@ -1,9 +1,43 @@
 import axios from 'axios';
+import store from '@/store';
+import router from '@/router';
 
 // const API_ENDPOINT = 'http://localhost:8000'
 // const API_ENDPOINT = 'http://3.66.167.5'
-const API_ENDPOINT = 'https://www.api-kpkdev.tk'
+// const API_ENDPOINT = 'https://www.api-kpkdev.tk'
+const API_ENDPOINT = 'http://3.69.239.163'
 
+
+const TOKEN_NAME = 'token';
+const SALT = 'eVhfwAhfwDfewuIfenwKbfuweLfbewiefOfejbwOefwuknqdX'
+
+function getToken() {
+  return JSON.parse(localStorage.getItem(TOKEN_NAME));
+}
+axios.interceptors.request.use(
+  (config) => {
+    if (getToken()) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${getToken() + SALT}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+axios.interceptors.response.use(
+  (success) => success,
+  (error) => {
+    if (error.response.data?.statusCode === 401) {
+      localStorage.removeItem(TOKEN_NAME);
+      store.state.auth.user = null;
+      store.state.loader.isLoading = false;
+      router.push({ path: '/' });
+    }
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return Promise.reject(error?.response?.data);
+  },
+);
 export default {
 
     get(url, params = {}, headers = {}) {
