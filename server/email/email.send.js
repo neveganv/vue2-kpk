ed73cf22fc75df5b7b03e65d44440eda64cbc26e
neveganv <victorianastiuk@gmail.com>
2022-10-25 16:45:18 +0300

    fixed email sending

--- server/email/email.send.js
+++ server/email/email.send.js
@@ -1,35 +1,64 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.GMAIL_USER,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN
+    }
+  });
+
+  return transporter;
+};
 
 async function sendEmail(email, pass, subj) {
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.GMAIL_USER, 
-      pass: process.env.GMAIL_PASS, 
-    },
-  });
+  let emailTransporter = await createTransporter();
 
   //text 
   let html = "<br>Ваш email для входу: " + email + "<br>Ваш пароль для входу: " + pass
 
   // send mail 
-    const info = await transporter.sendMail({
-      from: process.env.GMAIL_USER, // sender address
-      to: email, // receiver
-      subject: subj, // Subject line
-      html: html, // html body
-    });
+  const info = await emailTransporter.sendMail({
+    from: process.env.GMAIL_USER, // sender address
+    to: email, // receiver
+    subject: subj, // Subject line
+    html: html, // html body
+  });
 
-  
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
 }
 
